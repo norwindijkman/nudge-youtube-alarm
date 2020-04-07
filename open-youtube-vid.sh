9019e1b7c030da8e76e8546fda49584189ab2b77
Coffee-Z1 <=>
2020-04-07 19:14:38 +0200

    :hammer: browser command in .env

--- open-youtube-vid.sh
+++ open-youtube-vid.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+. "${0%/*}"/.env
 VIDEO_ID=$("${0%/*}"/venv/bin/python "${0%/*}"/youtube-video-id-gen.py $1)
 VIDEO_URL="https://youtube.com/watch?v=${VIDEO_ID}"
-google-chrome-stable $VIDEO_URL
+$BROWSER $VIDEO_URL
